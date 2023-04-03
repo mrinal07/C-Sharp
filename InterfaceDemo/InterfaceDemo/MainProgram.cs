6f810dd0c494d8478f0bf5dc2eae3de921a231dc
Mrinal Choudhary <41827353+mrinal07@users.noreply.github.com>
2023-04-03 12:49:35 +0530

    Add files via upload

---
+++ InterfaceDemo/InterfaceDemo/MainProgram.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 1) An interface defines a signature of methods, events and indexes but not an implementation.
+
+// 2) The interface members don't have any access specifier, by default they are public.
+
+// 3) By default an interface is internal and we can't declare it private, protected or
+// protected internal because it is always inherited by classes but we can use public.
+
+// 4) An interface does not have fields; in other words we can't declare variables in an interface.
+// default interface feature is there in C# 8.0 or higher
+
+// 5) Should use a public access specifier for inherited members from an interface to a class.
+// By default all members are public in interfaces but when we implement a method in a class
+// we need to define it as public.
+
+// 
+
+
+namespace InterfaceDemo
+{
+    internal class MainProgram:Demo
+    {
+        static void Main(string[] args)
+        {
+            // Implicit
+            Shape shape = new Shape();
+            shape.DrawShape();
+            shape.ColorTheShape();
+            Console.WriteLine();
+
+            // Another way of calling
+
+            IShape obj = new Shape();
+            obj.DrawShape();
+            obj.ColorTheShape();
+            Console.WriteLine();
+
+
+
+            // Explicit
+
+            // Error below
+            //Shape2 shape2 = new Shape2();
+            //shape2.DrawShape();
+            //shape2.ColorTheShape();
+
+
+            // An explicitly implemented member cannot be accessed using a class instance,
+            // but only through an instance of the interface.
+
+            // The main reason for explicit interface definitions is to avoid naming
+            // conflicts if you happen to implement two interfaces that contain methods with the same signature...
+            // the explicit definition allows the compiler to keep the signatures distinct enough to resolve.
+            // It is more readable and eliminates the confusion.
+            // It is also useful if interfaces have the same method name coincidently.
+
+
+            IShape obj2 = new Shape2();
+            obj2.DrawShape();
+            obj2.ColorTheShape();
+            Console.WriteLine();
+
+            //IShape shape1; // Use???
+
+
+            MainProgram p = new MainProgram();
+            p.DrawShape();
+            
+
+        }
+
+        public override void Output()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InterfaceDemo/InterfaceDemo/Shape.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 1 Why we cannot create obj of abstract class and interface
+
+// 2 why by deafult public in interface, if public then any one can access this
+
+// 3 After writing public before method in interface still we get error
+// Ans: Interface is a contract and anywhere where you can access the interface,
+// you should be able to access all the methods in it. In other words, all the
+// methods declared in the interface are supposed to be public so it doesn't make
+// sense stating it explicitly.
+
+// 4 can we create constructor in abstract class, if yes then tell its type
+
+namespace InterfaceDemo
+{
+    // Only interface
+    interface IShape
+    {
+        //string Name { get; set; }
+         void DrawShape();
+        void ColorTheShape();
+    }
+    class Shape : IShape
+    {
+        //string IShape.Name { get ; set; }
+
+        // Error below: because Name is private in IShape
+        string Name { get ; set; }
+
+        public void ColorTheShape()
+        {
+            Console.WriteLine("Inside Shape Class");
+            Console.WriteLine("Coloring the rectangle.");
+            
+        }
+
+        public void DrawShape()
+        {
+            Console.WriteLine("Inside Shape Class");
+            Console.WriteLine("Drawing rectangle");
+        }
+    }
+
+    class Shape2 : IShape
+    {
+        void IShape.ColorTheShape()
+        {
+            Console.WriteLine("Inside Shape2 Class");
+            Console.WriteLine("Coloring the rectangle.");
+        }
+
+        void IShape.DrawShape()
+        {
+            Console.WriteLine("Inside Shape2 Class");
+            Console.WriteLine("Drawing rectangle");
+        }
+    }
+
+    // Multiple Inheritance using Interface
+
+    class Shape3 : Shape, IShape
+    {
+        public new void ColorTheShape()
+        {
+            Console.WriteLine("Inside Shape3 Class");
+            Console.WriteLine("Coloring the rectangle.");
+
+        }
+
+        public new void DrawShape()
+        {
+            Console.WriteLine("Inside Shape3 Class");
+            Console.WriteLine("Drawing rectangle");
+        }
+        public void Display()
+        {
+            Console.WriteLine("Inside Shape3 Class Display method");
+        }
+    }
+
+
+    // Implementing Interface in abstract class
+    abstract class Demo : IShape
+    {
+        int id;
+        public Demo()
+        {
+            id = 101;
+            Console.WriteLine("Abstract Class constructor "+id);
+        }
+        public void ColorTheShape()
+        {
+            Console.WriteLine("Inside abstract class Demo");
+            Console.WriteLine("Coloring the rectangle.");
+        }
+
+        public void DrawShape()
+        {
+            Console.WriteLine("Inside abstract class Demo");
+            Console.WriteLine("Drawing rectangle");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Inside Shape3 Class Display method");
+        }
+
+        abstract public void Output();
+        
+    }
+
+
+
+
+}
